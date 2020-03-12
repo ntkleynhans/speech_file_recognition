29e95c7219a7172e9176d85ca1172479c5755856
Neil Kleynhans <ntkleynhans@gmail.com>
2020-03-12 06:43:50 +0200

    add bash rec runner

---
+++ run_rec.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+WAV_FILE=$1
+TAG=$2
+
+#Run command
+
+exit $?
--- speechApp.py
+++ speechApp.py
@@ -15,6 +15,23 @@ def root():
     else:
         return jsonify({"message": "Unauthorized"}), 401
 
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    headers = request.headers
+    print(headers)
+    auth = headers.get("X-API-KEY")
+    if auth != os.getenv('X_API_KEY'):
+        return jsonify({"message": "Unauthorized"}), 401
+
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        return jsonify({"message": "Bad Request - no file part in POST"}), 400
+    file = request.files['file']
+    if file:
+        tf = f'{tempfile.NamedTemporaryFile().name}.wav'
+        file.save(tf)
+        return jsonify({"message": f"File saved!"}), 200
+
 @app.route('/recognize', methods=['POST'])
 def upload_file():
     headers = request.headers
